588cce21f35a7552f6ceadc553f0d33bf8ef94bb
Oscar Garcia <ogarcia2834@outlook.com>
2022-07-09 12:03:25 -0500

    q5 table added

--- src/main/scala/Project1/Spark.scala
+++ src/main/scala/Project1/Spark.scala
@@ -50,7 +50,13 @@ object Spark {
     spark.sql("DROP TABLE IF EXISTS Query4")
     spark.sql("create table Query4(Year Int,Month Int,`Total Covid-19 Death` Int) row format delimited fields terminated by ',' stored as textfile")
     spark.sql("INSERT INTO TABLE Query4 (SELECT Year, Month, SUM(`COVID-19 Deaths`) AS `Total Covid-19 Death` FROM Main WHERE Month IS NOT NULL GROUP BY Month,Year ORDER BY Year,Month ASC  )")
-    spark.sql("Select * From Query4").show(50)
+    //spark.sql("Select * From Query4").show(50)
+
+
+    spark.sql("DROP TABLE IF EXISTS Query5")
+    spark.sql("create table Query5(State String,SEX String,`Age Group` String,`Other Causes of Death` Int) row format delimited fields terminated by ',' stored as textfile")
+    spark.sql("INSERT INTO TABLE Query5(SELECT State,Sex,`Age Group`,(`Total Deaths`-`COVID-19 Deaths`) AS `Other Causes of Death` From Test WHERE `Age Group` = 'All Ages' AND Sex = 'All Sexes' AND Group = 'By Total' ORDER BY `Other Causes of Death` ASC)")
+    spark.sql("Select * From Query5").show(50)
 
 
 
